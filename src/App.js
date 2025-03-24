ae04750a12f10947c0e20c9c8ba1e9b0c4d09012
Malu1313 <marialuisavinholireis@gmail.com>
2025-03-24 15:04:24 -0300

    24-02

--- src/App.js
+++ src/App.js
@@ -1,17 +1,29 @@
-import React from 'react';
-import { NavigationContainer } from '@react-navigation/native';
-import { createStackNavigator } from '@react-navigation/stack';
+import React from "react";
+import { NavigationContainer } from "@react-navigation/native";
+import { createStackNavigator } from "@react-navigation/stack";
 import Login from "./screens/Login";
-import Cadastro from './screens/Cadastro';
+import Cadastro from "./screens/Cadastro";
+import Layout from "./components/Layout";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Login">
-        <Stack.Screen name="Login" component={Login} />
-        <Stack.Screen name="Cadastro" component={Cadastro} />
+      <Stack.Navigator>
+        <Stack.Screen
+          name="Login"
+          component={() => (
+            <Layout>
+              <Login />
+            </Layout>
+          )}
+        />
+        <Stack.Screen name="Cadastro" component={()=>(
+          <Layout>
+            <Cadastro/>
+          </Layout>
+        )} />
       </Stack.Navigator>
     </NavigationContainer>
   );
